f7452a00fe9e0595df7c99a715b7847184f0be49
Oxelie <stephanieduhem.simplon@gmail.com>
2026-07-15 19:31:10 +0200

    modif api url

--- app.py
+++ app.py
@@ -1,8 +1,11 @@
+import os
 import streamlit as st
 import requests
 import base64
 
-list_img = requests.get("http://127.0.0.1:4444/list_img").json()
+API_URL = os.getenv("API_URL", "http://127.0.0.1:4444")
+
+list_img = requests.get(f"{API_URL}/list_img").json()
 
 
 st.title("Welcome to the Home page!")
@@ -10,7 +13,7 @@ st.title("Welcome to the Home page!")
 # selected = st.selectbox("Select a image", options=list_img, format_func=lambda x: x["path"])
 selected = st.selectbox("Select a image", options=list_img, format_func=lambda x: x["path"].split("/")[-1])
 
-response = requests.post("http://127.0.0.1:4444/select_img", json={"image_index": selected["index"]})
+response = requests.post(f"{API_URL}/select_img", json={"image_index": selected["index"]})
 
 image_orig = base64.b64decode(response.json()["image"])
 mask = base64.b64decode(response.json()["mask"])
@@ -20,10 +23,10 @@ with col1:
     st.image(image_orig)
 with col2:
     st.image(mask)
-    
-    
+
+
 col1, col2, col3 = st.columns([2,1,2])
-    
+
 with col2:
     st.markdown("""
                     <style>
@@ -38,13 +41,13 @@ with col2:
                     }
                     </style>
                 """, unsafe_allow_html=True)
-    
+
     gros_bouton = st.button("§ PREDICT §")
 
 
 col1, col2, col3 = st.columns([1,3,1])
 with col2:
     if gros_bouton:
-        response = requests.post("http://127.0.0.1:4444/predict", json={"image_index": selected["index"]})
+        response = requests.post(f"{API_URL}/predict", json={"image_index": selected["index"]})
         mask_pred = base64.b64decode(response.json()["img_mask_pred"])
         st.image(mask_pred)
